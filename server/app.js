7bee92f7e0f73324b6cb3fef8513986ace75a37c
Jimmy Mutuku <jimmymutuku1@gmail.com>
2024-03-07 16:41:23 +0300

    Implemented the signin functionality

--- server/app.js
+++ server/app.js
@@ -1,7 +1,10 @@
 import express from 'express';
+import dotenv from 'dotenv';
 import connectToDatabase from './db';
 import authRoutes from './routes/auth';
 
+
+dotenv.config();
 
 async function startServer() {
     const app = express();
--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,7 +1,8 @@
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 import User from '../models/User';
-import connectToDatabase from '../db';
 
+// Signup controller
 export const signup = async (req, res) => {
     const { username, email, password } = req.body;
 
@@ -13,17 +14,61 @@ export const signup = async (req, res) => {
 
         const hashedPassword = await bcrypt.hash(password, 10);
 
-        const db = await connectToDatabase();
-
-        await db.collection('users').insertOne({
+	const newUser = new User({
             username,
             email,
             password: hashedPassword,
         });
+	await newUser.save();
 
         return res.status(201).json({ message: 'User created successfully.' });
     } catch (error) {
         console.error('Error signing up user:', error);
         return res.status(500).json({ message: 'Internal server error' });
+    }
+};
+
+
+// Login controller
+export const login = async (req, res) => {
+    try {
+	const { email, password } = req.body;
+
+	const user = await User.findOne({ email });
+	if (!user) {
+	    return res.status(404).json({
+		message: 'User not found'
+	    });
+	}
+
+	const passwordMatch = await bcrypt.compare(password, user.password);
+	if (!passwordMatch) {
+	    return res.status(401).json({
+		message: 'Invalid credentials'
+	    });
+	}
+
+	const token = jwt.sign(
+	    { userId: user._id },
+	    process.env.JWT_SECRET,
+	    { expiresIn: '1h' });
+
+	res.status(200).json({ token });
+    } catch (error) {
+	console.error(error);
+	res.status(500).json({
+	    message: 'Internal server error'
+	});
+    }
+};
+
+// Logout controller (not implemented yet)
+export const logout = async (req, res) => {
+    try {
+	// Logout logic to be implemented here
+	res.status(200).json({ message: 'Logout successful' });
+    } catch (error) {
+	console.error(error);
+	res.status(500).json({ message: 'Internal server error' });
     }
 };
